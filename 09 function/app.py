d2fafea8b8956013fbd4122f0666e5d65fadab5e
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-03-29 00:21:34 +0600

    chap 9 adding

---
+++ 09 function/app.py
@@ -0,0 +1,18 @@
+# def is defiant a function
+def my_fun():
+    print("hello")
+    print("working ")
+
+
+print("start")
+my_fun()
+print('finish')
+
+# ----------pera mitar----------
+
+
+def users(name):
+    print(f'hi {name} !')
+
+
+users("rifat")
