493b1ba3b536a1eeda8e9b75813346b305c8135c
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-03-29 00:16:09 +0600

    chap 8 update

---
+++ 08 dictionary/num_game.py
@@ -0,0 +1,18 @@
+num = input("Number > ")
+dics = {
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+}
+
+output = ""
+for ch in num:
+    output += dics.get(ch, "!") + " "
+print(output)
