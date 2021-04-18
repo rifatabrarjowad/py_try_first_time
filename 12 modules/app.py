ed755f7c27ec91496087092285e8343f1630a7f2
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-18 23:03:41 +0600

    adding modules

---
+++ 12 modules/app.py
@@ -0,0 +1,6 @@
+import convart
+
+
+x = convart.kg_to_lbs(70)
+
+print(x)
---
+++ 12 modules/random.py
@@ -0,0 +1,8 @@
+import random
+
+# for i in range(3):
+#     print(random.randint(10, 20))
+member = ['Abrar', 'Rifat', 'Jowad']
+
+led = random.choice[member]
+print(led)
